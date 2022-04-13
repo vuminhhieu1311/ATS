3fc30a4d422e8ca5177afa998a03b5bd21bc5bf6
Vu Minh Hieu <vuminhhieu1311@gmail.com>
2022-04-13 22:55:13 +0700

    feat: set up project

---
+++ web/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#7367f0',
+                success: '#28c76f',
+                danger: '#ea5455',
+                warning: '#ff9f43',
+                info: '#909399',
+                black: '#22292f',
+                white: '#FFFFFF',
+            },
+        },
+    },
+};
